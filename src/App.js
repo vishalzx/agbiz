25db89d91f4c8c87c7774f8ff9596f652814fd41
vishalzx <108291592+vishalzx@users.noreply.github.com>
2023-12-22 12:52:55 +0530

    scommit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 // import './App.css';
 // import Home from './Components/Assignment1/Home';
-import Home from "./Components/Assignment3/Home";
+import Home from "./Components/Assignment2/Home";
+// import Home from "./Components/Assignment3/Home";
 // import Home from "./Components/Assignment4/Home";
+
 
 
 function App() {
---
+++ src/Components/Assignment2/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Home.css";
+
+const Home=()=>{
+    return(
+        <>
+            <h1 style={{textAlign:"center"}}>Top Courses to Study in India</h1>
+            <p style={{width:"80%", margin:"auto"}}>India has a profitable economy and a high academic system. As a result, it is a good choice for students to study India. Selecting the right course after the Intermediate is an important decision for students. Among all courses, Engineering is on top of the list among Indian students. Candidates can search for the best engineering colleges in India and apply there easily.</p>
+            <p style={{width:"80%", margin:"auto", marginTop:"15px"}}>There are so many undergraduate and post graduate programs available in Engineering, Management, and Medical for students.</p>
+    
+        </>
+    );
+}
+
+export default Home;
